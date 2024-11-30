30e00566fdd3c58e300e02cace9c1f66e3e8ada8
vccaso <vccaso@scartarissolutions.com>
2024-11-30 15:52:46 -0500

    [WIP] add initial example

--- arduino/library/max7219/example001.ino
+++ arduino/library/max7219/example001.ino
@@ -1,2 +1,53 @@
-// MAX7219
+//MAX7219 - using Led Control library to display all   leds one by one
+#include <LedControl.h>
+
+int DIN = 12;
+int CS = 10;
+int   CLK = 11;
+
+LedControl lc=LedControl(DIN, CLK, CS,0);
+
+
+
+void setup()   {
+
+  lc.shutdown(0,false);
+  lc.setIntensity(0,0);
+  lc.clearDisplay(0);
+}
+
+void   loop() {
+  E();
+  T();
+}
+
+void  E() {  
+  lc.clearDisplay(0);
+  lc.setLed(0,1,1,true);
+  lc.setLed(0,1,2,true);
+  lc.setLed(0,1,3,true);
+  lc.setLed(0,1,4,true);
+  lc.setLed(0,1,5,true);
+  lc.setLed(0,2,1,true);
+  lc.setLed(0,3,1,true);
+  lc.setLed(0,2,3,true);
+  lc.setLed(0,3,3,true);
+  lc.setLed(0,2,5,true);
+  lc.setLed(0,3,5,true);
+  delay(1000);
+}
+
+void T() {
+  lc.clearDisplay(0);
+  lc.setLed(0,3,1,true);
+  lc.setLed(0,3,2,true);
+  lc.setLed(0,3,3,true);
+  lc.setLed(0,3,4,true);
+  lc.setLed(0,3,5,true);
+  lc.setLed(0,2,5,true);
+  lc.setLed(0,1,5,true);
+  lc.setLed(0,4,5,true);
+  lc.setLed(0,5,5,true);
+  delay(1000);
+}
 
